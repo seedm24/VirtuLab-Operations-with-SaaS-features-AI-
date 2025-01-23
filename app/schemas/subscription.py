0b607ba3d959695a708c7ab9c414f67d9588c214
seedm24 <despotic62@gmail.com>
2025-01-24 01:20:18 +0300

    Update subscription.py

--- app/schemas/subscription.py
+++ app/schemas/subscription.py
@@ -1,1 +1,14 @@
+from pydantic import BaseModel
+
+class SubscriptionRequest(BaseModel):
+    name: str
+    price: int
+    features: list[str]
+
+class SubscriptionResponse(BaseModel):
+    id: int
+    name: str
+    price: int
+    features: list[str]
+    is_active: bool
 
