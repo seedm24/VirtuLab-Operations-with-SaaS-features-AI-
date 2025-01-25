6b88f7b7044972faeebef259c6c844cf1db0d66b
seedm24 <despotic62@gmail.com>
2025-01-25 09:59:31 +0300

    Update seo.py

--- app/models/seo.py
+++ app/models/seo.py
@@ -1,1 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from app.database.db import Base
+
+class SEO(Base):
+    __tablename__ = 'seo_entries'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    keywords = Column(String)
 
