397c7de3f192e33cee59422fbbfe441085a365bc
seedm24 <despotic62@gmail.com>
2025-01-25 09:58:48 +0300

    Update agent.py

--- app/models/agent.py
+++ app/models/agent.py
@@ -1,1 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from app.database.db import Base
+
+class Agent(Base):
+    __tablename__ = 'agents'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String)
 
